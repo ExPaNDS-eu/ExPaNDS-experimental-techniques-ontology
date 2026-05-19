2f94904bec54387c764f7f60af7edae24b186870
kara-mela <Melanie.Nentwich@desy.de>
2026-05-19 09:23:22 +0200

    Update visible comments on onto spec page
    Motivation
    
    The onto spec page is still work in progress and contains visible comments on
    upcoming tasks (to do).
    
    Modification
    
    These comments have been updated to make them more professional

--- docs/_data/owl2json.py
+++ docs/_data/owl2json.py
@@ -3,20 +3,6 @@ import json
 
 # Load the OWL file (RDF/XML format)
 g = Graph()
-g.parse("PaNET.owl", format="xml")  # Replace with your file path
-
-# Serialize to JSON-LD (standardized RDF-to-JSON format)
-jsonld_data = g.serialize(format="json-ld")
-
-# Save to a JSON file
-with open("PaNET.json", "w") as f:
-    f.write(jsonld_data)
-
-print("Conversion of PaNET.owl complete! Output saved to PaNET.json")
-
-
-# Load the OWL file (RDF/XML format)
-g = Graph()
 g.parse("PaNET_reasoned.owl", format="xml")  # Replace with your file path
 
 # Serialize to JSON-LD (standardized RDF-to-JSON format)
@@ -27,18 +13,3 @@ with open("PaNET_reasoned.json", "w") as f:
     f.write(jsonld_data)
 
 print("Conversion of PaNET_reasoned.owl complete! Output saved to PaNET_reasoned.json")
-
-
-
-# Load the Turtle file
-g = Graph()
-g.parse("PaNET_metadata.ttl", format="turtle")
-
-# Serialize to JSON-LD
-jsonld_data = g.serialize(format="json-ld", indent=2)
-
-# Save to a file
-with open("PaNET_metadata.json", "w") as f:
-    f.write(jsonld_data)
-
-print("Conversion PaNET_metadata.ttl complete! Output saved to metadata.json")
